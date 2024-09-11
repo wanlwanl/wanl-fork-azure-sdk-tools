bb600661403b5d6c5338a10d30dd496cd9a79e11
wanl <wanl@microsoft.com>
2024-09-11 13:17:53 +0800

    Separate tests

--- tools/js-sdk-release-tools/packages/typescript-codegen-breaking-change-detector/src/tests/patch-rule.test.ts
+++ tools/js-sdk-release-tools/packages/typescript-codegen-breaking-change-detector/src/tests/patch-rule.test.ts
@@ -38,8 +38,39 @@ describe('patch basic breaking changes', async () => {
     }
   });
 
-  test('detect interface breaking change', async () => {
-    const testCaseDir = '../../misc/test-cases/patch-basic-detection/include-interface';
+  test('detect interface\'s breaking changes for include-interface rule', async () => {
+    const testCaseDir = '../../misc/test-cases/patch-basic-detection/include-interface/interface-level';
+    const currentPackageFolder = join(__dirname, testCaseDir, 'current-package');
+    const baselinePackageFolder = join(__dirname, testCaseDir, 'baseline-package');
+    const date = getFormattedDate();
+    let tempFolder = '';
+    try {
+      tempFolder = await createTempFolder(`.tmp/temp-${date}`);
+      const messagesMap = await detectBreakingChangesBetweenPackages(
+        [RuleIds.includeInterface],
+        baselinePackageFolder,
+        currentPackageFolder,
+        tempFolder
+      );
+
+      console.log('-----------res-----', messagesMap);
+      expect(messagesMap.size).toBe(1);
+      messagesMap.forEach((messages) => {
+        messages?.forEach((message) => {
+          expect(message.kind).toBe(RuleMessageKind.PatchMessage);
+          const patchMessage = message as PatchMessage;
+          expect(patchMessage.incompatibleTypeAlias).toBeUndefined();
+        });
+      });
+    } catch (err) {
+      throw err;
+    } finally {
+      await remove(tempFolder);
+    }
+  });
+
+  test('detect children\'s of interface breaking changes for include-interface rule', async () => {
+    const testCaseDir = '../../misc/test-cases/patch-basic-detection/include-interface/property-level';
     const currentPackageFolder = join(__dirname, testCaseDir, 'current-package');
     const baselinePackageFolder = join(__dirname, testCaseDir, 'baseline-package');
     const date = getFormattedDate();
