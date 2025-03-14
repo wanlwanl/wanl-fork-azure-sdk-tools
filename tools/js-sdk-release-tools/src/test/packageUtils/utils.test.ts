e3dbde58eedaae102d8638a604f0e36407785772
wanl <wanl@microsoft.com>
2025-03-14 18:07:53 +0800

    add test cases

---
+++ tools/js-sdk-release-tools/src/test/packageUtils/utils.test.ts
@@ -0,0 +1,59 @@
+import { expect, describe, it } from "vitest";
+import { hasOperations } from "../../utils/typespecUtils";
+import { SDKType } from "../../common/types";
+import { resolve } from "path";
+
+interface Case {
+    description: string;
+    packageRoot: string;
+    sdkType: SDKType;
+    expected: boolean;
+}
+
+describe("detect operations in specification", () => {
+    const cases: Case[] = [
+        {
+            description: "should return true on HLC",
+            packageRoot: resolve(
+                __dirname,
+                "testCases/highLevelClient/specification"
+            ),
+            sdkType: SDKType.HighLevelClient,
+            expected: true,
+        },
+        {
+            description: "should return false on RLC with operations",
+            packageRoot: resolve(
+                __dirname,
+                "testCases/restLevelClient/withOperations"
+            ),
+            sdkType: SDKType.RestLevelClient,
+            expected: true,
+        },
+        {
+            description: "should return false on RLC without operations",
+            packageRoot: resolve(__dirname, "testCases/withoutOperations"),
+            sdkType: SDKType.RestLevelClient,
+            expected: false,
+        },
+        {
+            description: "should return true on Modular client with operations",
+            packageRoot: resolve(
+                __dirname,
+                "testCases/modularClient/withOperations"
+            ),
+            sdkType: SDKType.ModularClient,
+            expected: true,
+        },
+        {
+            description:
+                "should return false on Modular client without operations",
+            packageRoot: resolve(__dirname, "testCases/withoutOperations"),
+            sdkType: SDKType.ModularClient,
+            expected: false,
+        },
+    ];
+    it.each(cases)("$description", async (c: Case) => {
+        expect(await hasOperations(c.packageRoot, c.sdkType)).toBe(c.expected);
+    });
+});
